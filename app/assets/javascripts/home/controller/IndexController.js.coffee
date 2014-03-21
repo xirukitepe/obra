1180d152db5ecb8937e7c1c10dd0d724bccd9c9a
Gerda Decio <gerdadecio@gmail.com>
2014-03-22 03:10:01 +0800

    git status

--- app/assets/javascripts/home/controller/IndexController.js.coffee
+++ app/assets/javascripts/home/controller/IndexController.js.coffee
@@ -1,5 +1,4 @@
 Obra.controller("IndexController", ($scope, $location, homeData) ->
-  $scope.hello = "haha"
   homeData.homePath().query (result) ->
     $scope.hot = result.hot
     $scope.new_releases = result.new_release
--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -9,10 +9,6 @@ class PortfoliosController < ApplicationController
   def list
   	@comics = Comic.comics_of(params[:user_id])
     @user = User.find params[:user_id]
-
-    Rails.logger.info "---------------"
-    Rails.logger.info @user.custom_hash
-    Rails.logger.info "---------------"
     render json: {comics: @comics.map(&:custom_hash), user: @user.custom_hash}
   end
 
